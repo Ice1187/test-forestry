22f56385104294504f26b2bb1c2663974d1a3812
Ice1187 <Ice1187@users.noreply.github.com>
2022-04-26 08:24:25 +0000

    jekyll build from Action 8a3abf4f1564c7db94a06feb9f5e73f5d2237773

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,169 @@
+var store = [{
+        "title": "Lhasa Apso",
+        "excerpt":"The Lhasa Apso (/ˈlɑːsə ˈæpsoʊ/ lah-sə ap-soh) is a non-sporting dog breed originating in Tibet. It was bred as an interior sentinel in the Buddhist monasteries, to alert the monks to any intruders who entered. Lhasa is the capital city of Tibet, and apso is a word in the Tibetan...","categories": [],
+        "tags": [],
+        "url": "/test-forestry/pets/lhasa-apso/",
+        "teaser": null
+      },{
+        "title": "Tabby",
+        "excerpt":"A tabby is any domestic cat that has a coat featuring distinctive stripes, dots, lines or swirling patterns, usually together with a mark resembling an ‘M’ on its forehead. Tabbies are sometimes erroneously assumed to be a cat breed. In fact, the tabby pattern is found in many breeds, as...","categories": [],
+        "tags": [],
+        "url": "/test-forestry/pets/tabby/",
+        "teaser": null
+      },{
+        "title": "Post: Modified Date",
+        "excerpt":"This post has been updated and should show a modified date if used in a layout. All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a...","categories": ["Post Formats"],
+        "tags": ["Post Formats","readability","standard"],
+        "url": "/test-forestry/post%20formats/post-modified/",
+        "teaser": null
+      },{
+        "title": "Post: Standard",
+        "excerpt":"All children, except one, grow up. They soon know that they will grow up, and the way Wendy knew was this. One day when she was two years old she was playing in a garden, and she plucked another flower and ran with it to her mother. I suppose she...","categories": ["Post Formats"],
+        "tags": ["Post Formats","readability","standard"],
+        "url": "/test-forestry/post%20formats/post-standard/",
+        "teaser": null
+      },{
+        "title": "Post: Chat",
+        "excerpt":"Abbott: Strange as it may seem, they give ball players nowadays very peculiar names. Costello: Funny names? Abbott: Nicknames, nicknames. Now, on the St. Louis team we have Who’s on first, What’s on second, I Don’t Know is on third– Costello: That’s what I want to find out. I want...","categories": ["Post Formats"],
+        "tags": ["chat","Post Formats"],
+        "url": "/test-forestry/post%20formats/post-chat/",
+        "teaser": null
+      },{
+        "title": "Post: Notice",
+        "excerpt":"A notice displays information that explains nearby content. Often used to call attention to a particular detail. When using Kramdown {: .notice} can be added after a sentence to assign the .notice to the &lt;p&gt;&lt;/p&gt; element. Changes in Service: We just updated our privacy policy here to better service our...","categories": ["Post Formats"],
+        "tags": ["Post Formats","notice"],
+        "url": "/test-forestry/post%20formats/post-notice/",
+        "teaser": null
+      },{
+        "title": "Post: Quote",
+        "excerpt":"   Only one thing is impossible for God: To find any sense in any copyright law on the planet.       Mark Twain   ","categories": ["Post Formats"],
+        "tags": ["Post Formats","quote"],
+        "url": "/test-forestry/post%20formats/post-quote/",
+        "teaser": null
+      },{
+        "title": "Post: Link",
+        "excerpt":"This theme supports link posts, made famous by John Gruber. To use, just add link: http://url-you-want-linked to the post’s YAML front matter and you’re done.      And this is how a quote looks.    Some link can also be shown.  ","categories": ["Post Formats"],
+        "tags": ["link","Post Formats"],
+        "url": "/test-forestry/post%20formats/post-link/",
+        "teaser": null
+      },{
+        "title": "Post: Video (YouTube)",
+        "excerpt":"YouTube video embed below.    ","categories": ["Post Formats"],
+        "tags": ["Post Formats"],
+        "url": "/test-forestry/post%20formats/post-video-youtube/",
+        "teaser": null
+      },{
+        "title": "Post: Header Image with OpenGraph Override",
+        "excerpt":"This post has a header image with an OpenGraph override.   header:   image: /assets/images/page-header-image.png   og_image: /assets/images/page-header-og-image.png  ","categories": ["Layout","Uncategorized"],
+        "tags": ["edge case","image","layout"],
+        "url": "/test-forestry/layout/uncategorized/post-header-image-og-override/",
+        "teaser": null
+      },{
+        "title": "Post: Image (Standard)",
+        "excerpt":"The preferred way of using images is placing them in the /assets/images/ directory and referencing them with an absolute path. Prepending the filename with {{ site.url }}{{ site.baseurl }}/assets/images/ will make sure your images display properly in feeds and such. Standard image with no width modifier classes applied. HTML: &lt;img...","categories": ["Post Formats"],
+        "tags": ["image","Post Formats"],
+        "url": "/test-forestry/post%20formats/post-image-standard/",
+        "teaser": null
+      },{
+        "title": "Post: Teaser Image with OpenGraph Override",
+        "excerpt":"This post has a teaser image with an OpenGraph override.   header:   teaser: /assets/images/page-header-teaser.png   og_image: /assets/images/page-header-og-image.png  ","categories": ["Layout","Uncategorized"],
+        "tags": ["edge case","image","layout"],
+        "url": "/test-forestry/layout/uncategorized/post-teaser-image-og-override/",
+        "teaser": "/test-forestry/assets/images/page-header-teaser.png"
+      },{
+        "title": "Post: Image (Linked with Caption)",
+        "excerpt":"          Image with a caption.   ","categories": ["Post Formats"],
+        "tags": ["image","Post Formats"],
+        "url": "/test-forestry/post%20formats/post-image-linked-caption/",
+        "teaser": null
+      },{
+        "title": "Post: Image (Caption)",
+        "excerpt":"         Photo from Unsplash.  ","categories": ["Post Formats"],
+        "tags": ["image","Post Formats"],
+        "url": "/test-forestry/post%20formats/post-image-caption/",
+        "teaser": null
+      },{
+        "title": "Post: Gallery",
+        "excerpt":"These are gallery tests for image wrapped in &lt;figure&gt; elements. To place a gallery add the necessary YAML Front Matter: gallery: - url: /assets/images/unsplash-gallery-image-1.jpg image_path: /assets/images/unsplash-gallery-image-1-th.jpg alt: \"placeholder image 1\" title: \"Image 1 title caption\" - url: /assets/images/unsplash-gallery-image-2.jpg image_path: /assets/images/unsplash-gallery-image-2-th.jpg alt: \"placeholder image 2\" title: \"Image 2 title caption\" -...","categories": ["Post Formats"],
+        "tags": ["gallery","Post Formats","tiled"],
+        "url": "/test-forestry/post%20formats/post-gallery/",
+        "teaser": null
+      },{
+        "title": "Layout: Header Image (External URL)",
+        "excerpt":"This post should display a header image, if the theme supports it.   Featured image is an external asset and should load.  ","categories": ["Layout","Uncategorized"],
+        "tags": ["edge case","featured image","image","layout"],
+        "url": "/test-forestry/layout/uncategorized/layout-header-image-external/",
+        "teaser": null
+      },{
+        "title": "Layout: Header Image (Horizontal)",
+        "excerpt":"This post should display a header image, if the theme supports it.   Non-square images can provide some unique styling issues.   This post tests a horizontal header image.  ","categories": ["Layout","Uncategorized"],
+        "tags": ["edge case","featured image","image","layout"],
+        "url": "/test-forestry/layout/uncategorized/layout-header-image-horizontal/",
+        "teaser": null
+      },{
+        "title": "Layout: Header Image and Text Readability",
+        "excerpt":"This is a sample post with a large feature image1 up top and tons of text. Odio ad blue bottle vinyl, 90’s narwhal commodo bitters pour-over nostrud. Ugh est hashtag in, fingerstache adipisicing laboris esse Pinterest shabby chic Portland. Shoreditch bicycle rights anim, flexitarian laboris put a bird on it...","categories": [],
+        "tags": ["sample post","readability","test"],
+        "url": "/test-forestry/layout-header-image-text-readability/",
+        "teaser": null
+      },{
+        "title": "Layout: Header Image (Vertical)",
+        "excerpt":"This post should display a header image, if the theme supports it.   Non-square images can provide some unique styling issues.   This post tests a vertical header image.  ","categories": ["Layout","Uncategorized"],
+        "tags": ["edge case","featured image","image","layout"],
+        "url": "/test-forestry/layout/uncategorized/layout-header-image-vertical/",
+        "teaser": null
+      },{
+        "title": "Layout: Header Overlay with Background Fill",
+        "excerpt":"This post should display a header with a solid background color, if the theme supports it.   Non-square images can provide some unique styling issues.   This post tests overlay headers.  ","categories": ["Layout","Uncategorized"],
+        "tags": ["edge case","image","layout"],
+        "url": "/test-forestry/layout/uncategorized/layout-header-overlay-color/",
+        "teaser": null
+      },{
+        "title": "Layout: Sidebar Custom",
+        "excerpt":"This post has a custom sidebar set in the post’s YAML Front Matter.   An example of how that YAML could look is:   sidebar:   - title: \"Title\"     image: http://placehold.it/350x250     image_alt: \"image\"     text: \"Some text here.\"   - title: \"Another Title\"     text: \"More text here.\"     nav: sidebar-sample  ","categories": [],
+        "tags": [],
+        "url": "/test-forestry/layout-sidebar-custom/",
+        "teaser": null
+      },{
+        "title": "Markup: HTML Tags and Formatting",
+        "excerpt":"A variety of common markup showing how the theme styles them. Header two Header three Header four Header five Header six Blockquotes Single line blockquote: Stay hungry. Stay foolish. Multi line blockquote with a cite reference: People think focus means saying yes to the thing you’ve got to focus on....","categories": ["Markup"],
+        "tags": ["content","css","formatting","html","markup"],
+        "url": "/test-forestry/markup/markup-html-tags-and-formatting/",
+        "teaser": "/test-forestry/assets/images/markup-syntax-highlighting-teaser.jpg"
+      },{
+        "title": "Markup: Syntax Highlighting",
+        "excerpt":"Syntax highlighting is a feature that displays source code, in different colors and fonts according to the category of terms. This feature facilitates writing in a structured language such as a programming language or a markup language as both structures and syntax errors are visually distinct. Highlighting does not affect...","categories": [],
+        "tags": ["code","syntax highlighting"],
+        "url": "/test-forestry/markup-syntax-highlighting/",
+        "teaser": "/test-forestry/assets/images/markup-syntax-highlighting-teaser.jpg"
+      },{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["Jekyll"],
+        "tags": ["update"],
+        "url": "/test-forestry/jekyll/welcome-to-jekyll/",
+        "teaser": "/test-forestry/assets/images/500x300.png"
+      },{
+        "title": "Testing",
+        "excerpt":"H1   H2   H3   H4   H5   H6   Bold   Italic   link      Quote     cool    import numpy as np     np.array([[1,2],[3,4]])   Section     Content      a   b   c           1       printf(\"indented code block\\n\");           2   3            a       b           Is this ok?  ","categories": ["test"],
+        "tags": ["test"],
+        "url": "/test-forestry/test/testing/",
+        "teaser": null
+      },{
+        "title": "Chocolate Chip Cookies",
+        "excerpt":"A chocolate chip cookie is a drop cookie that originated in the United States and features chocolate chips as its distinguishing ingredient. The traditional recipe combines a dough composed of butter and both brown and white sugar with semi-sweet chocolate chips. Variations include recipes with other types of chocolate as...","categories": [],
+        "tags": [],
+        "url": "/test-forestry/recipes/chocolate-chip-cookies/",
+        "teaser": null
+      },{
+        "title": "Oatmeal Cookies",
+        "excerpt":"Oatmeal cookies are a proverbial favorite with both kids and adults. This crisp and chewy cookie is loaded with oats, dried fruit, and chopped nuts. Ingredients 1 cup butter, softened 1 cup white sugar 1 cup packed brown sugar 2 eggs 1 teaspoon vanilla extract 2 cups all-purpose flour 1...","categories": [],
+        "tags": [],
+        "url": "/test-forestry/recipes/oatmeal-cookies/",
+        "teaser": null
+      },{
+        "title": "Peanut Butter Cookies",
+        "excerpt":"A peanut butter cookie is a type of cookie that is distinguished for having peanut butter as a principal ingredient. The cookie generally originated in the United States, its development dating back to the 1910s. Ingredients 1 cup unsalted butter 1 cup crunchy peanut butter 1 cup white sugar 1...","categories": [],
+        "tags": [],
+        "url": "/test-forestry/recipes/peanut-butter-cookies/",
+        "teaser": null
+      }]
